ad453b8a7e3769a2791057d242cd89c15d9a9407
Abnestinencia <abnermoraesdp@gmail.com>
2024-12-10 17:49:01 -0300

    erro

--- index.js
+++ index.js
@@ -4,33 +4,10 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true}))
 
+app.use(express.static('./paginas/public'))
+
 const porta = 7000;
 const host = "0.0.0.0";
-
-/*-- LOGIN --*/
-function login(req, res){
-    res.send(`<html>
-                <head>
-                    <title>Login</title>
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-                </head>
-                <body>
-                    <a class="navbar-brand" href="#">Login</a>
-                    <form method="POST" action="/">
-                    <div class="form-group">
-                        <label for="exampleInputName1">Nome</label>
-                        <input type="text" class="form-control" id="exampleInputName1" placeholder="Vitor" name="nome">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Senha</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="senha123" name="senha">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
-                    </form>
-                </body>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-              </html>`);
-}
 
 /*-- CADASTRO --*/
 
@@ -62,6 +39,41 @@ function cadastrar(req, res){
       </html>`);
 }
 
+function autenticarUsuario(req, resp){
+    const usuario = req.body.usuario;
+    const senha = req.body.senha;
+
+    if(usuario === '' && senha === ''){
+
+    }
+    else{
+        resp.write(`
+                    <head>
+                            <title>Tente novamente</title>
+                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                            <meta charset="utf-8">
+                        </head>
+                        <body>
+                            <div class="container w-25">
+                                <div class="alert alert-danger" role="alert">
+                                    Usuario ou senha invalidos
+                                </div>
+                                <div>
+                                    <a href="/login.html" class="btn btn-primary">Tentar Novamente</a>
+                                </div>
+                            </div>
+                        </body>
+                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                    </html>
+            `)
+    }
+}
+
+app.get('/', (req, res) =>{
+    resp.redirect('/login.html');
+});
+
+app.post('/', autenticarUsuario);
 app.get('/', login);
 app.get('/cadastrar', cadastrar);
 
