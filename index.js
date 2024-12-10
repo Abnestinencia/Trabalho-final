36005ca2f601b4f3d4ef9c0f8c7b6f3c1b1d4a40
Abnestinencia <abnermoraesdp@gmail.com>
2024-12-10 18:59:29 -0300

    Validação de log-in

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import session from "express-session";
 
 const app = express();
+
+app.use(session({
+    secret: '',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 30
+    }
+}))
 
 app.use(express.urlencoded({ extended: true}))
 
@@ -8,6 +18,15 @@ app.use(express.static('./paginas/public'))
 
 const porta = 7000;
 const host = "0.0.0.0";
+
+/*-- INICIO --*/
+function inicio(req, res){
+        resp.redirect('/login.html');
+}
+
+/*-- MENU --*/
+
+/*-- BATE-PAPO --*/
 
 /*-- CADASTRO --*/
 
@@ -18,7 +37,7 @@ function cadastrar(req, res){
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         </head>
         <body>
-            <form method="POST" action="/">
+            <form method="POST" action="/batepapo">
             <div class="form-group">
                 <label for="exampleInputName1">Nome</label>
                 <input type="text" class="form-control" id="exampleInputName1" placeholder="Paulo Ribeira" name="nome">
@@ -39,15 +58,18 @@ function cadastrar(req, res){
       </html>`);
 }
 
+/*-- AUTENTICAR --*/
+
 function autenticarUsuario(req, resp){
     const usuario = req.body.usuario;
     const senha = req.body.senha;
 
     if(usuario === '' && senha === ''){
-
+        req.session.usuarioLogado = true;
+        resp.redirect('/menu')
     }
     else{
-        resp.write(`
+        resp.send(`
                     <head>
                             <title>Tente novamente</title>
                             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -65,17 +87,33 @@ function autenticarUsuario(req, resp){
                         </body>
                         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
                     </html>
-            `)
+            `);
     }
 }
 
-app.get('/', (req, res) =>{
-    resp.redirect('/login.html');
+/*-- VALIDAR AUTENTICAÇÃO --*/
+function verificarAutent(req, resp, next){
+    if(req.session.usuarioLogado){
+        next();
+    }
+    else
+    {
+        resp.redirect('/login.html')
+    }
+}
+
+/*-- ROTAS --*/
+app.post('/cadastrar', (req, res) => {
+    const nome = req.body.nome;
+    const data = req.body.data;
+    const apelido = req.body.apelido;
+
+    res.send(`<h1>Cadastrado!</h1>`);
 });
 
-app.post('/', autenticarUsuario);
-app.get('/', login);
-app.get('/cadastrar', cadastrar);
+app.post('/login', autenticarUsuario);
+app.get('/', inicio);
+app.get('/cadastrar', verificarAutent cadastrar);
 
 app.listen(porta, host, () => {
     console.log(`Está no ar, Endereço: http://${host}:${porta}`);
