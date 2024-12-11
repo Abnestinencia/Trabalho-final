5bd59abf890e537ea5c46fc0475149e99857bfca
Abnestinencia <abnermoraesdp@gmail.com>
2024-12-10 21:44:02 -0300

    Atualização 3

--- index.js
+++ index.js
@@ -25,9 +25,14 @@ app.use(express.static(path.join(process.cwd(), 'paginas/public')));
 const porta = 7000;
 const host = "localhost";
 
+var listaUsuarios = [];
 
 /*-- MENU --*/
 function menu(req, res){
+    const dataHoraUltimoLogin = req.cookies['dataHoraUltimoLogin'];
+    if(!dataHoraUltimoLogin){
+        dataHoraUltimoLogin='';
+    }
     res.send(`<html>
                 <head>
                     <title>Login</title>
@@ -63,6 +68,12 @@ function menu(req, res){
                         <li class="nav-item">
                         <a class="nav-link" href="#">Bate-Papo</a>
                         </li>
+                        <li class="nav-item">
+                        <a class="nav-link" class="btn btn-danger" href="/logout">Sair</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Seu ultimo acesso foi realizado em ${dataHoraUltimoLogin}</a>
+                        </li>
                     </ul>
                     <!-- Left links -->
                     </div>
@@ -88,7 +99,7 @@ function cadastrar(req, res){
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         </head>
         <body>
-            <form method="POST" action="/batepapo">
+            <form method="POST" action="/cadastrar">
             <div class="form-group">
                 <label for="exampleInputName1">Nome</label>
                 <input type="text" class="form-control" id="exampleInputName1" placeholder="Paulo Ribeira" name="nome">
@@ -105,6 +116,7 @@ function cadastrar(req, res){
             <button type="submit" class="btn btn-primary">Cadastrar</button>
             </form>
         </body>
+        <a class="btn btn-secondary" href="/">Voltar para o Menu</a>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
       </html>`);
 }
@@ -114,6 +126,87 @@ function cadastrarUsuario(req, resp){
     const nome = req.body.nome;
     const data = req.body.data;
     const apelido = req.body.apelido;
+
+    if(nome && data && apelido){
+        const usuario = {nome, data, apelido}
+
+        listaUsuarios.push(usuario);
+
+        resp.write(`<html>
+                        <head>
+                            <title>Cadastrado com Sucesso</title>
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <meta charset="utf-8">
+                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                        </head>
+                        <body>
+                            <h1>Usuario Cadastrado!</h1>
+                            <a class="btn btn-primary" href="/cadastrar">Continuar Cadastrando</a>
+                            <a class="btn btn-secondary" href="/">Voltar para o Menu</a>
+                        </body>
+                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                    </html>
+                    `)
+        
+    }
+    else{
+        resp.write(`<html>
+        <head>
+            <title>Login</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta charset="utf-8">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        </head>
+        <body>
+            <form method="POST" action="/cadastrar">
+            <div class="form-group">
+                <label for="exampleInputName1">Nome</label>
+                <input type="text" class="form-control" id="exampleInputName1" placeholder="Paulo Ribeira" name="nome" value="${nome}">
+            </div>
+        `);
+        if(!nome){
+            resp.write(`
+                <div>
+                    <span><p class="bg-danger">Por favor, você deve informar o nome do Usuario</p></span>
+                </div>
+                `);
+        }
+        resp.write(`
+            <div class="form-group">
+                <label for="exampleInputName2">Data de Nascimento</label>
+                <input type="date" class="form-control" id="exampleInputDate1" placeholder="28/06/2004" name="data">
+            </div>
+            `);
+        if(!data){
+            resp.write(`
+                <div>
+                    <span><p class="bg-danger">Por favor, você deve informar a data do Usuario</p></span>
+                </div>
+                    `);
+            }
+        resp.write(`
+            <div class="form-group">
+                <label for="exampleInputName3">Nickname/Apelido</label>
+                <input type="text" class="form-control" id="exampleInputName3" placeholder="Zé" name="apelido">
+            </div>
+            `);
+        if(!data){
+            resp.write(`
+                <div>
+                    <span><p class="bg-danger">Por favor, você deve informar o apelido do Usuario</p></span>
+                </div>
+                    `);
+                }
+        resp.write(`
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </form>
+            </body>
+            <a class="btn btn-secondary" href="/">Voltar para o Menu</a>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+            </html>
+            `)
+
+    }
 
 
 }
@@ -165,9 +258,14 @@ function verificarAutent(req, resp, next){
 
 /*-- ROTAS --*/
 
+app.get('/logout', (req, resp) => {
+    req.session.destroy();
+    resp.redirect('/login.html');
+})
 app.post('/login', autenticarUsuario);
 app.get('/', verificarAutent, menu);
 app.get('/cadastrar', verificarAutent, cadastrar);
+app.post('/cadastrar', cadastrarUsuario);
 
 app.listen(porta, host, () => {
     console.log(`Está no ar, Endereço: http://${host}:${porta}`);
