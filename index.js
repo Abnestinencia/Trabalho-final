1450696b58d169fd61f7618381630dfb37652cb7
Abnestinencia <abnermoraesdp@gmail.com>
2024-12-10 22:44:19 -0300

    Final.

--- index.js
+++ index.js
@@ -26,8 +26,10 @@ const porta = 7000;
 const host = "localhost";
 
 var listaUsuarios = [];
+var mensagens = [];
 
 /*-- MENU --*/
+
 function menu(req, res){
     const dataHoraUltimoLogin = req.cookies['dataHoraUltimoLogin'];
     if(!dataHoraUltimoLogin){
@@ -66,7 +68,7 @@ function menu(req, res){
                         <a class="nav-link" href="/cadastrar">Cadastrar Usuarios</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="#">Bate-Papo</a>
+                        <a class="nav-link" href="/batepapo">Bate-Papo</a>
                         </li>
                         <li class="nav-item">
                         <a class="nav-link" class="btn btn-danger" href="/logout">Sair</a>
@@ -89,7 +91,69 @@ function menu(req, res){
 }
 
 /*-- BATE-PAPO --*/
+function batepapo(req, res) {
+    const usuarioSelecionado = req.query.usuario || "";
+    res.write(`
+    <html>
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta charset="utf-8">
+            <title>Bate-Papo</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+        </head>
+        <body>
+            <div class="container">
+                <h2>Bate-Papo</h2>
+                <form action="/enviaMensagem" method="POST">
+                    <div class="form-group">
+                        <label for="usuario">Escolha o Usuário:</label>
+                        <select name="usuario" class="form-control" required>
+                            <option value="">Selecione Usuario</option>`);
+                                for (let i = 0; i < listaUsuarios.length; i++) {
+                                    let usuario = listaUsuarios[i];
+                                    res.write(`<option value="${usuario.apelido}">${usuario.apelido}</option>`)
+                                }
+                        res.write(`</select>
+                    </div>
+                    <div class="form-group mt-2">
+                        <label for="mensagem">Mensagem:</label>
+                        <textarea name="mensagem" class="form-control" rows="4" required></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-3">Enviar Mensagem</button>
+                    <a class="btn btn-secondary" href="/">Voltar para o Menu</a>
+                </form>
 
+                <h3>Mensagens Enviadas:</h3>
+                <div class="list-group">`)
+                        for (let i = 0; i < mensagens.length; i++) {
+                            let msg = mensagens[i];
+                            res.write(`
+                                <div class="list-group-item">
+                                    <strong>${msg.usuario}</strong> (em ${msg.dataHora}): <p>${msg.mensagem}</p>
+                                </div>
+                                `);
+                        }
+                res.write(`</div>
+            </div>
+        </body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </html>`);
+}
+
+
+
+/*-- ENVIAR MENSAGEM --*/
+app.post('/enviaMensagem', (req, res) => {
+    const { usuario, mensagem } = req.body;
+
+    if (usuario && mensagem) {
+        const dataHora = new Date().toLocaleString();
+        mensagens.push({ usuario, mensagem, dataHora });
+        res.redirect('/batepapo');
+    } else {
+        res.send('Mensagem ou Usuario não selecionado!');
+    }
+});
 /*-- CADASTRO --*/
 
 function cadastrar(req, res){
@@ -132,21 +196,40 @@ function cadastrarUsuario(req, resp){
 
         listaUsuarios.push(usuario);
 
-        resp.write(`<html>
-                        <head>
-                            <title>Cadastrado com Sucesso</title>
-                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                            <meta charset="utf-8">
-                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-                        </head>
-                        <body>
-                            <h1>Usuario Cadastrado!</h1>
-                            <a class="btn btn-primary" href="/cadastrar">Continuar Cadastrando</a>
-                            <a class="btn btn-secondary" href="/">Voltar para o Menu</a>
-                        </body>
-                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-                    </html>
-                    `)
+        resp.write(`
+            <html>
+              <head>
+                <title>Lista de Usuarios</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta charset="utf-8">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+              </head>
+              <body>
+              <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Data</th>
+                        <th scope="col">Apelido</th>
+                    </tr>
+                </thead>
+                <tbody>`);
+    
+            for (var i = 0; i < listaUsuarios.length; i++) {
+                resp.write(`<tr>
+                                    <td>${listaUsuarios[i].nome}</td>
+                                    <td>${listaUsuarios[i].data}</td>
+                                    <td>${listaUsuarios[i].apelido}</td>
+                               </tr>`);
+            }
+    
+            resp.write(`</tbody>
+              </table>
+              <a class="btn btn-primary" href="/cadastrar">Continuar Cadastrando</a>
+              <a class="btn btn-secondary" href="/">Voltar para o Menu</a>
+              </body>
+              <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+            </html>`);
         
     }
     else{
@@ -265,6 +348,7 @@ app.get('/logout', (req, resp) => {
 app.post('/login', autenticarUsuario);
 app.get('/', verificarAutent, menu);
 app.get('/cadastrar', verificarAutent, cadastrar);
+app.get('/batepapo', verificarAutent, batepapo)
 app.post('/cadastrar', cadastrarUsuario);
 
 app.listen(porta, host, () => {
